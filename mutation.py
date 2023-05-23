18ce8162f0fd6dc2616a02b5f3b489a7f1ae617a
Diego Romero <diego.romero@unleashtechnologies.net>
2023-05-23 15:11:04 -0600

    Item Delete

--- mutation.py
+++ mutation.py
@@ -16,3 +16,11 @@ class Mutation:
         await populate_items(count)
         items = await Item.all().values()
         return [ItemType(**item) for item in items]
+
+    @strawberry.mutation
+    async def item_delete(self, id: ID) -> ItemType:
+        item = await Item.get(id=id).values()
+        item_type = ItemType(**item)
+        temp = await Item.get(id=id)
+        await temp.delete()
+        return item_type
